070e14d936e3bd1f0e91cb59cb0d38bb14253ae6
Dax <limeside@gmail.com>
2016-04-23 14:33:01 -0500

    Add basic dice roll functionality in script

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  $("main section h1").text("Sup.");
+  $("#roller").on('click',function(){
+    var $numDice = $("#num-dice").val();
+    var $randomRange = $("#text-field").val();
+    var totalRoll = 0;
+    var $output = $('main section h2');
+
+    if (typeof $randomRange !== 'int' || typeof $numDice !== 'int'){
+      $output.text("Enter an integer.");
+    } else {
+      for(i=0;i<$numDice;i++){
+        totalRoll += Math.floor((Math.random()*$randomRange)+1);
+        $output.text(totalRoll);
+        //TODO: Push each result into an array, list.
+      }
+    }
+  });
 });
