616784d77e68655c9f267a679105bea91ab93ed6
Dax <limeside@gmail.com>
2016-04-23 16:33:23 -0500

    Add visual feedback for dice rolls. Fixed stylesheet link.

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ $(document).ready(function(){
     var $randomRange = $("#text-field").val();
     var totalRoll = 0;
     var $output = $('main section h2');
+    var rollOutput = [];
 
-    if (typeof $randomRange !== 'int' || typeof $numDice !== 'int'){
-      $output.text("Enter an integer.");
-    } else {
-      for(i=0;i<$numDice;i++){
-        totalRoll += Math.floor((Math.random()*$randomRange)+1);
-        $output.text(totalRoll);
-        //TODO: Push each result into an array, list.
-      }
+    $("#dice-output").html("");
+
+
+    for(i=0;i<$numDice;i++){
+      var thisRoll = 0;
+
+      thisRoll = Math.floor( ( Math.random() * $randomRange ) + 1 );
+      rollOutput.push(thisRoll);
+
+      totalRoll += thisRoll;
+
+      $output.text(totalRoll);
+
+      //TODO: Push each result into an array, list.
+      $("#dice-output").append("<div class=\"die\"><h3>" + rollOutput[i] + "</h3></div>").hide().fadeIn('slow');
     }
   });
 });
